31f6ef6469fc0485bca3a415d7f853daf0ea6aa6
prabinnnn <prabinbhandari120@gmail.com>
2024-01-28 02:01:24 +0545

    add index

--- route/index.js
+++ route/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const userrouter = require("./user.route");
+const bookrouter = require("./book.route");
+const bookindex = "/api/v1";
 const userindex = "/api/v1";
 router.get("/", (res, req) => {
   res.json({ msg: "hello from prabein" });
 });
+router.use(`${bookindex}`, bookrouter);
 router.use(`${userindex}`, userrouter);
 module.exports = router;
