e12aec087be7775b2450bbaab9a953a95e12640a
prabinnnn <prabinbhandari120@gmail.com>
2024-01-28 15:58:40 +0545

    add index

---
+++ route/book.route.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+router.get("/", (res, req) => {
+  res.json({ msg: "hello from prabein" });
+});
+router.put("/:id", (res, req, next) => {
+  try {
+    const { id } = req.param;
+    if (!id) throw new ERROR("something else");
+    res.json({ msg: "create a new book" });
+  } catch (e) {
+    next(e);
+  }
+});
+router.post("/", (res, req) => {
+  res.json({ msg: "hello from book" });
+});
+router.patch("/", (res, req, next) => {
+  try {
+    Object.keys(({ id } = req.body)).length;
+    if (!id) throw new ERROR("something else");
+    res.json({ msg: "update the book" });
+  } catch (e) {
+    next(e);
+  }
+});
+router.delete("/", (res, req, next) => {
+  try {
+    Object.keys(({ id } = req.body)).length;
+    if (!id) throw new ERROR("something else");
+    res.json({ msg: "delete  the book" });
+  } catch (e) {
+    next(e);
+  }
+});
+module.exports = router;
